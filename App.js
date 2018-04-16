56931838ddc6cc1ba3e250dc6b46fbe6e7a6d7e1
Ken Chen <lbkchen@gmail.com>
2018-04-16 01:03:52 -0700

    Removes firebase admin, going to just use live database.g

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { firebase } from './config/firebase'; 
+
 export default class App extends React.Component {
   render() {
     return (
